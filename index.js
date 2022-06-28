b250563371ce08ef90bf70bd114075beea757c60
KPatel <KPatel@AUTO2019>
2022-06-28 11:57:03 -0400

    Made changes to footer, skills, project section and added nav scroll time line animation.

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
+
 function collapsibleClickEventHandler(){
     var collapsible = document.getElementsByClassName("collapsible");
 
     for(let i = 0; i < collapsible.length; i++){
         collapsible[i].addEventListener("click", function(){
             this.classList.toggle("active");
-            console.log("clicked")
             var content = this.nextElementSibling;
             if (content.style.maxHeight) {
+                content.style.paddingBottom = "0px";
                 content.style.maxHeight = null;
             }else{
+                content.style.paddingBottom = "10px";
                 content.style.maxHeight = content.scrollHeight + "px";
             }
         });
@@ -29,5 +32,28 @@ function formInputValueHandler(){
     }
 }
 
+function scrollStyleHandler(){
+    const navScrollTracker = document.querySelector(".scroll-tracker");
+
+    const scrollTimeline = new ScrollTimeline({
+        source: document.scrollingElement,
+        orientation: "block",
+        scrollOffsets: [CSS.percent(0), CSS.percent(100)]
+    });
+
+    navScrollTracker.animate(
+        {
+            transform: ["scaleX(0)", "scaleX(1)"]
+        },
+        {
+            duration: 1,
+            timeline: scrollTimeline
+        }
+    );
+
+}
+
+
+scrollStyleHandler();
 collapsibleClickEventHandler();
 formInputValueHandler();
